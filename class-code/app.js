9fe99afc822c121e24d62f0cab53feeb2e0b3207
omarakamal <omaraakamal@gmail.com>
2025-11-11 16:12:13 +0300

    update

---
+++ class-code/app.js
@@ -0,0 +1,112 @@
+
+// Declaring an Array
+// Ordered Collection of values
+
+const myStudents = ['Abdulrahman','Nawaf','Noora']
+
+
+console.log(myStudents)
+
+// access a value in my array
+
+console.log(myStudents[0])
+
+
+
+// .length
+console.log(myStudents.length)
+
+
+// pop(): Remove the last value in the array
+myStudents.pop()
+
+console.log(myStudents)
+
+
+// push(): add an element to the end of the array
+
+myStudents.push('Ali')
+
+console.log(myStudents)
+
+
+// shift(): removes the first element
+
+myStudents.shift()
+
+console.log(myStudents)
+
+
+// unshift(): adds to the beggening of array
+myStudents.unshift('Mohammad')
+
+console.log(myStudents)
+
+
+const techonologies = ['HTML','CSS','JavaScript']
+
+
+techonologies.push('Python')
+techonologies.unshift('Node')
+
+console.log(techonologies)
+
+const techologiesCopy = techonologies
+
+techologiesCopy.push('ALI')
+
+console.log('technology', techonologies)
+
+
+console.log('technologyCppy', techologiesCopy)
+
+
+console.log(myStudents)
+// splice(startingPoint, deleteCount)
+
+myStudents.splice(0,1)
+
+console.log(myStudents)
+
+
+myStudents.splice(1,1,'new 1','new 1','new 3')
+
+console.log(myStudents)
+
+
+function something(){
+    return 5
+}
+
+console.log(something())
+
+
+const myNums = [1,5,10,20]
+const secondNums = [20,30,40,50]
+
+
+console.log(myNums[1] +secondNums[0])
+
+
+myStudents.splice(1,3)
+myStudents.splice(1,0,'Munther','Shahad')
+
+
+console.log(myStudents)
+
+
+// console.log(myStudents[2])
+
+// Looping through an array
+for(let i = 0; i< myStudents.length; i++ ){
+    console.log(myStudents[i])
+}
+
+
+// for of loop
+
+for( let anything of myStudents){
+  console.log(anything)  
+}
+
+
